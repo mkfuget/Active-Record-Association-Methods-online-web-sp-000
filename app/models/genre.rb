ead944eaf9d289460922666f2caa2081a4508731
Michael Fuget <michaelfuget@gmail.com>
2020-12-16 00:13:12 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.songs.map{|song| song.artist}.uniq.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.songs.map{|song| song.artist.name}.uniq
   end
 end
